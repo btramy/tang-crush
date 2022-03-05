d770b04bd066b209c7017acf092d468d710ccd6b
btramy <101003976+btramy@users.noreply.github.com>
2022-03-05 21:19:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Uầy uầy uây uây
-Sao mới gặp lần đầu mà đầu mình quay quay
-Anh ơi anh à
-Anh bỏ bùa gì mà lại làm em yêu vậy?`,
-    btnIntro: 'hihi',
+    introTitle: 'Bae à!',
+    introDesc: `bấm tiếp để thấy nhiều điều hay ho nha hehee
+    btnIntro: '😙',
     title: 'Thứ 3 này anh rảnh hem 😙',
     desc: 'Đi chơi với em nha ',
     btnYes: 'Đi luôn <33',
-    btnNo: 'K đi đâu :3',
+    btnNo: 'K đi đâu bận lắm
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại thích em <3',
     btnReply: 'Gửi em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu :)))',
-    mess: 'Em biết mà 🥰. Yêu bae nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
     messDesc: 'Hẹn bae t3 gặp nhé',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
